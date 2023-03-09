7160737a41bda6d49213357a4565e8f330a0791d
Vanessa Foerster <nessfoerster@gmail.com>
2023-03-08 20:26:32 -0500

    wip: adds button for login on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+// import { login } from '../firebase';
+// import { Link } from 'react-router-dom';
+
 function Home (props) {
-    return <h1>Home Page</h1>;
+    return (
+        <>
+        <h1>Down to Match Home Page</h1>
+        <button /*onClick={login}*/>Signup or Login!</button>
+        </>
+
+    )
   }
   
   export default Home;
