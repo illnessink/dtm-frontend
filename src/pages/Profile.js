d1fbc298b9118198456b40afb06c1e7b4b74f7a2
Molly Raik <molly.raik@gmail.com>
2023-03-08 20:26:34 -0500

    wip: profile show page architecture

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+
 function Profile (props) {
-    return <h1> Profile Page</h1>;
+    return (
+        <div >
+            <h1>{props.profile.displayName}</h1>
+            <img src={props.profile.photo} alt={props.profile.displayName} />
+            <h3>{props.profile.age} yrs</h3>
+            <h3>{props.profile.location}</h3>
+            <h3>Looking to meet: {props.profile.interestedIn}</h3>
+            <h2>About Me:</h2>
+            <p>{props.profile.bio}</p>
+            <h2>Fun Fact!</h2>
+            <p>{props.profile.funFact}</p>
+            <h2>Hobbies:</h2>
+            <ul>
+                { props.profile.hobbies.map((hobby) => {
+                    return <li>{hobby}</li>
+                })
+                }
+            </ul>
+
+        </div>
+    
+    );
 }
 
 export default Profile;
