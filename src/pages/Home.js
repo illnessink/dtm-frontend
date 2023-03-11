e027fde1e4fc9c0228674fe690adf19e950bb8a4
Vanessa Foerster <nessfoerster@gmail.com>
2023-03-11 12:55:08 -0500

    wip: adds button for profile if already logged in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-// import { login } from '../firebase';
-// import { Link } from 'react-router-dom';
+import { login } from '../firebase';
+import { Link } from 'react-router-dom';
 
 function Home (props) {
     return (
         <>
-        <h1>Down to Match Home Page</h1>
-        <button /*onClick={login}*/>Signup or Login!</button>
+            <h1>Down to Match Home Page</h1>
+        { !props.user ?
+            
+            <button onClick={login}>Signup or Login!</button>
+            :
+            <Link to={`/profile/${props.user.uid}`}>
+                <button>Go To Profile!</button>
+            </Link>
+        }
         </>
 
     )
