8a67eeb614b3c65a8e240c84b36cfe627b75be43
mollyraik <molly.raik@gmail.com>
2023-03-14 21:43:02 -0400

    wip: chat code part 2

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3001");
+
 function Chat (props) {
+    const [message, setMessage] = useState("");
+
+    const sendMessage = () => {
+      socket.emit("send_message", {
+        message
+      })
+    }
+
+    useEffect(() => {
+      socket.on("received_message", (data) => {
+        alert(data.message);
+      })
+    }, [socket])
+
     return (
     <div className="chat-feature" >
       <ul id="messages"></ul>
       <form id="form" action="">
-        <input id="input" autocomplete="off" /><button>Send</button>
+        <input id="input" autocomplete="off" placeholder="Message..."/><button onClick={sendMessage}>Send</button>
       </form>
     </div> 
     );
