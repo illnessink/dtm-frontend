b48b255b90e9e7809fdc6d95981839b855629e31
danaurys <danaurysperez@gmail.com>
2023-03-14 21:07:42 -0400

    updates quiz questions

--- src/pages/json.js
+++ src/pages/json.js
@@ -1,19 +1,93 @@
 export const json = {
     "elements": [{
+   /* {
       "type": "boolean",
       "name": "slider",
       "title": "Are you 21 or older?",
       "description": "Display mode = Default",
       "valueTrue": "Yes",
       "valueFalse": "No"
-    }, {
+    }, 
+    */
       "type": "boolean",
-      "name": "radiobutton",
-      "title": "Are you 21 or older?",
-      "description": "Display mode = Radio",
+      "name": "q1",
+      "title": "Do you like to read?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q2",
+      "title": "Do you like sports?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q3",
+      "title": "Does money bring you happiness?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q4",
+      "title": "Would you change cities to be with a partner?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q5",
+      "title": "Are you pursuing a monogamous relationship?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q6",
+      "title": "Do you have any pets?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q7",
+      "title": "Do you believe in magic?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q8",
+      "title": "Do you want children?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q9",
+      "title": "Would you cheat on a test?",
+      //"description": "Display mode = Radio",
+      "valueTrue": "Yes",
+      "valueFalse": "No",
+      "renderAs": "radio"
+    },{
+      "type": "boolean",
+      "name": "q10",
+      "title": "Do you believe in a higher power?",
+      //"description": "Display mode = Radio",
       "valueTrue": "Yes",
       "valueFalse": "No",
       "renderAs": "radio"
     }],
-    "showQuestionNumbers": false
+    "showQuestionNumbers": true
   };
