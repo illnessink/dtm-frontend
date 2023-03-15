392593245ae566e933612f828e26a9c3a475255a
mollyraik <molly.raik@gmail.com>
2023-03-15 19:43:33 -0400

    wip: sends quiz data to backend

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -18,6 +18,7 @@ import { json } from "./json";
   export default Quiz;
   */
 
+
 import React from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
@@ -25,11 +26,23 @@ import "survey-core/defaultV2.min.css";
 //import "./index.css";
 import { json } from "./json";
 
-function Quiz() {
+function Quiz(props) {
     const quiz = new Model(json);
-    quiz.onComplete.add((sender, options) => {
-        console.log(JSON.stringify(sender.data, null, 3));
-    });
+    const API_URL = "http://localhost:3001/quizzes/";
+    quiz.onComplete.add( async (sender, options) => {
+        if (props.user){
+          const token = await props.user.getIdToken();
+          // sender.data.uid = props.user.uid;
+          await fetch(API_URL + props.user.uid, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'Application/json',
+              'Authorization': 'Bearer ' + token,
+            },
+            body: JSON.stringify(sender.data),
+          });    
+      }
+      });
     return (<Survey model={quiz} />);
 }
 
