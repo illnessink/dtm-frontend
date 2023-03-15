14a4d88e2ab4d43d6daf127a465d0225b61a1dfa
mollyraik <molly.raik@gmail.com>
2023-03-14 21:14:44 -0400

    wip: devloping chat feature

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3001");
+
 function Chat (props) {
+    const sendMessage = () => {
+      socket.emit("send_message", {
+        message: "hello",
+      })
+    }
+
+    useEffect(() => {
+      socket.on("received_message", (data) => {
+        alert(data.message);
+      })
+    }, [socket])
+
     return (
     <div className="chat-feature" >
       <ul id="messages"></ul>
       <form id="form" action="">
-        <input id="input" autocomplete="off" /><button>Send</button>
+        <input id="input" autocomplete="off" placeholder="Message..."/><button onClick={sendMessage}>Send</button>
       </form>
     </div> 
     );
