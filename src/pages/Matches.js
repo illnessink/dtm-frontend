fe3e86a9fb2e65f2468b9e1f715157f937f7006c
mollyraik <molly.raik@gmail.com>
2023-03-15 22:01:39 -0400

    wip: working on quiz compatibility logic

--- src/pages/Matches.js
+++ src/pages/Matches.js
@@ -1,6 +1,40 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState, useCallback } from 'react';
 
 function Matches (props) {
+  // const [quizzes, setQuizzes] = useState(null);
+
+  // const getQuizzes = async () =>{
+  //   try {
+  //     const token = await props.user.getIdToken();
+  //     const response = await fetch("http://localhost:3001/quizzes/", {
+  //       method: 'GET',
+  //       headers: {
+  //         'Authorization': 'Bearer ' + token
+  //       }
+  //     });
+  //     const data = await response.json();
+  //     setQuizzes(data);
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // };
+
+  // const userQuiz = quizzes.find(quiz => quiz.uid === props.user.uid)
+
+  // const checkCompatibility = (uid) => {
+  //   const matchQuiz = quizzes.find(quiz => quiz.uid === uid);
+  //   let compatibilityScore = 0;
+  //   console.log("match quiz", matchQuiz);
+  //   console.log("user quiz", userQuiz);
+  // }
+
+  // useEffect(()=>{
+  //     console.log(props.user)
+  //     getQuizzes();
+    
+  // }, [props.user]);
+
   // loaded function
   const loaded = () => {
     return props.profiles.map((profile) => (
@@ -11,6 +45,7 @@ function Matches (props) {
         </Link>
         <img src={profile.photo} alt={profile.displayName} />
         <h3>Age: {profile.age}</h3>
+        {/* {checkCompatibility(profile.uid)} */}
         </div>
       </div>
     ))
