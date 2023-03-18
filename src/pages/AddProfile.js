45d743f733c5d7e2f115eea467c4c8f949939444
mollyraik <molly.raik@gmail.com>
2023-03-18 10:47:46 -0400

    wip: loading loaded function for new profile

--- src/pages/AddProfile.js
+++ src/pages/AddProfile.js
@@ -47,103 +47,114 @@ function AddProfile(props) {
 
   // }
 
-  function handleOpenWidget(){
+  const loaded = () => {
+    function handleOpenWidget(){
 
-    const myWidget = window.cloudinary.createUploadWidget(
-      {
-      cloudName: 'dwr8ggqzr',
-      uploadPreset: 'carlhuv9',
-    },
-     (error, result) => {
-        if (!error && result && result.event === "success") {
-          // setImages((prev)=>[...prev,{url:result.info.url, public_id: result.info.public_id}])
-          setNewProfile((formData) =>({...formData, photo: result.info.secure_url}));
+      const myWidget = window.cloudinary.createUploadWidget(
+        {
+        cloudName: 'dwr8ggqzr',
+        uploadPreset: 'carlhuv9',
+      },
+       (error, result) => {
+          if (!error && result && result.event === "success") {
+            // setImages((prev)=>[...prev,{url:result.info.url, public_id: result.info.public_id}])
+            setNewProfile((formData) =>({...formData, photo: result.info.secure_url}));
+          }
         }
-      }
+      );
+      myWidget.open();
+    }
+
+    return (
+      <div className="container">
+        <button id="upload-widget" className='cloudinary-button' onClick={()=>handleOpenWidget()}>
+            Upload Picture
+          </button>
+         <form onSubmit={handleSubmit}>
+  
+  
+          <div className='images-preview-container'>
+          <div className='image-preview'>
+            <img src={newProfile.photo} alt={props.user.displayName}/>
+            {/* {imageToRemove !=image.public_id && <i className="fa fa-times close-icon" onClick={()=> handleRemoveImg()}></i>} */}
+            </div>
+  
+        </div>
+  
+  
+          <input
+            type="text"
+            value={newProfile.displayName}
+            name="displayName"
+            onChange={handleChange}
+          />
+  
+          <input
+            type="text"
+            value={newProfile.age}
+            name="age"
+            placeholder="age"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.location}
+            name="location"
+            placeholder="location"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.gender}
+            name="gender"
+            placeholder="gender"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.interestedIn}
+            name="interestedIn"
+            placeholder="Looking to meet..."
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.bio}
+            name="bio"
+            placeholder="A little about me..."
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.funFact}
+            name="funFact"
+            placeholder="Fun fact about me..."
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            value={newProfile.hobbies}
+            name="hobbies"
+            placeholder="please separate hobbies by a ',' "
+            onChange={handleChange}
+          />
+          <input type="submit" value="Create Profile" />
+        </form>
+  
+      </div>
     );
-    myWidget.open();
+  }
+
+  const loading = () => {
+    return <h1>Loading...</h1>
   }
 
   return (
-    <div className="container">
-      <button id="upload-widget" className='cloudinary-button' onClick={()=>handleOpenWidget()}>
-          Upload Picture
-        </button>
-       <form onSubmit={handleSubmit}>
+    <>
+    { props.user ? loaded() : loading() }
+    </>
+  )
 
-
-        <div className='images-preview-container'>
-        <div className='image-preview'>
-          <img src={newProfile.photo} alt={props.user.displayName}/>
-          {/* {imageToRemove !=image.public_id && <i className="fa fa-times close-icon" onClick={()=> handleRemoveImg()}></i>} */}
-          </div>
-
-      </div>
-
-
-        <input
-          type="text"
-          value={newProfile.displayName}
-          name="displayName"
-          onChange={handleChange}
-        />
-
-        <input
-          type="text"
-          value={newProfile.age}
-          name="age"
-          placeholder="age"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.location}
-          name="location"
-          placeholder="location"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.gender}
-          name="gender"
-          placeholder="gender"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.interestedIn}
-          name="interestedIn"
-          placeholder="Looking to meet..."
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.bio}
-          name="bio"
-          placeholder="A little about me..."
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.funFact}
-          name="funFact"
-          placeholder="Fun fact about me..."
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newProfile.hobbies}
-          name="hobbies"
-          placeholder="please separate hobbies by a ',' "
-          onChange={handleChange}
-        />
-        <input type="submit" value="Create Profile" />
-      </form>
-
-    </div>
-
-
-  );
 }
 
 export default AddProfile;
